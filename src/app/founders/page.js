0129af3847fbaa8d6bcd45bab49d1b19d0491b4d
Jose Luis Koller <joseluiskoller98@gmail.com>
2024-07-22 20:28:19 -0300

    extra stuff

--- src/app/founders/page.js
+++ src/app/founders/page.js
@@ -1,44 +1,43 @@
+// page.js
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import data from "./founder.json";
 
-function page() {
+function Page() {
   return (
-    <main className='m-1'>
-      <Link href='/'>{"<-"}</Link>
-      <ul className='flex flex-col gap-1 w-full text-center'>
-        <li>
-          <Link href='https://x.com/lautidev_'>Acosta, Lautaro</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/GuidooIrigoyen'>Irigoyen, Guido</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/0xKoller'>Koller, Jose Luis</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/montonenico'>Montone, Nicolás</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/fpedaccio'>Pedaccio, Facundo</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/software_valen'>Radovich, Valentin</Link>
-        </li>
-        <li>
-          <Link href='https://x.com/vsratti'>Ratti, Valentin </Link>
-        </li>
-        <li>
-          <Link href='https://x.com/alejorrojass'>Rojas, Alejo </Link>
-        </li>
-        <li>
-          <Link href='https://x.com/SantiagoSirvana'>Ruberto, Santiago </Link>
-        </li>
-        <li>
-          <Link href='https://x.com/mateozaratef'>Zarate, Mateo</Link>
-        </li>
-      </ul>
+    <main className='m-1 flex flex-col place-items-center h-screen'>
+      <Link className='absolute left-24' href='/'>
+        {"<-"}
+      </Link>
+      <h1 className='text-2xl font-bold text-center mb-4'>Founders</h1>
+      <div className='flex flex-wrap gap-x-12 gap-y-4 md:gap-x-12 md:gap-y-20 w-full md:w-2/4 justify-center'>
+        {data.map((founder) => {
+          const [lastName, firstName] = founder.name.split(", ");
+          return (
+            <Link
+              key={founder.id}
+              className='flex flex-col place-items-center border-2 hover:shadow-neutral-600 hover:border-neutral-700 transition-all duration-200 border-neutral-800 bg-neutral-900 hover:shadow-xl shadow-md shadow-neutral-700 p-2 rounded-md w-[150px] text-center md:w-[200px]'
+              href={`https://x.com/${founder.twitter}`}
+            >
+              <Image
+                src={founder.image}
+                alt={founder.name}
+                className='rounded-full mb-3'
+                width={70}
+                height={70}
+              />
+              <span>
+                <strong>{lastName}</strong>
+                <br />
+                {firstName}
+              </span>
+            </Link>
+          );
+        })}
+      </div>
     </main>
   );
 }
 
-export default page;
+export default Page;
--- src/app/members/page.js
+++ src/app/members/page.js
@@ -1,7 +1,68 @@
+// page.js
 import React from "react";
+import data from "./member.json";
+import Link from "next/link";
 
-function page() {
-  return <div></div>;
+function Page() {
+  const quantity = data.length;
+  return (
+    <main className='flex flex-col place-items-center m-1 p-2'>
+      <Link className='absolute left-24' href='/'>
+        {"<-"}
+      </Link>
+      <h1 className='text-2xl font-bold text-center mb-2'>Members</h1>
+      <div className='md:w-2/3 p-2'>
+        <div className='grid gap-2 md:grid-cols-2 mb-2'>
+          <p>
+            En esta sección se encuentran los miembros del O(n) Club, quienes
+            contribuyen con su experiencia y conocimientos al mismo. Si deseas
+            contactar a alguno de ellos, puedes hacerlo a través de sus redes
+            sociales.
+          </p>
+          <div className='hidden text-center md:flex flex-col place-items-center gap-2'>
+            <p>Si deseas unirte al club, puedes hacerlo haciendo click</p>
+            <Link
+              className='text-neutral-800 pointer-events-none flex flex-col place-items-center border-2 hover:shadow-neutral-600 hover:border-neutral-700 transition-all duration-200 border-neutral-800 bg-neutral-900 hover:shadow-xl shadow-md shadow-neutral-700 p-2 rounded-md w-[150px] text-center md:w-[200px]'
+              href='google.com'
+            >
+              Unirse a O(n) Club
+            </Link>
+          </div>
+        </div>
+        <p className='text-center md:text-end me-1 mb-2'>
+          Cantidad de miembros: {quantity}/20{" "}
+        </p>
+
+        <div className='flex flex-wrap gap-2  justify-evenly '>
+          {data.map((member) => {
+            const [lastName, firstName] = member.name.split(", ");
+            return (
+              <Link
+                href={"https://x.com/" + member.twitter}
+                key={member.name}
+                className='flex flex-col place-items-center border-2 hover:shadow-neutral-600 hover:border-neutral-700 transition-all duration-200 border-neutral-800 bg-neutral-900 hover:shadow-xl shadow-md shadow-neutral-700 p-2 rounded-md w-[150px] text-center md:w-[200px]'
+              >
+                <span>
+                  <strong>{lastName}</strong>
+                  <br />
+                  {firstName}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+        <div className='flex text-center md:hidden flex-col place-items-center gap-2 mt-8'>
+          <p>Si deseas unirte al club, puedes hacerlo haciendo click</p>
+          <Link
+            className='flex flex-col place-items-center border-2 hover:shadow-neutral-600 hover:border-neutral-700 transition-all duration-200 border-neutral-800 bg-neutral-900 hover:shadow-xl shadow-md shadow-neutral-700 p-2 rounded-md w-[150px] text-center md:w-[200px]'
+            href='google.com'
+          >
+            Unirse a O(n) Club
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default page;
+export default Page;
