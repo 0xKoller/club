f033c31b1db9691268b9e95030392dfbb2396f0c
Jose Luis Koller <joseluiskoller98@gmail.com>
2024-07-22 20:44:56 -0300

    twitter card

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,21 @@ export const metadata = {
   title: "O(n) Club",
   description:
     "Como Argentinos no tenemos que mirar afuera para buscar referentes",
+
+  url: "https://www.theonclub.com/",
+  image: "./favicons/android-chrome-512x512.png",
+  site_name: "O(n) Club",
+  images: {
+    default: "https://i.imgur.com/sU5ypgr.png",
+    alt: "O(n) Club",
+  },
+  twitter: {
+    card: "website",
+    title: "O(n) Club",
+    description:
+      "Como Argentinos no tenemos que mirar afuera para buscar referentes",
+    images: ["https://i.imgur.com/sU5ypgr.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
